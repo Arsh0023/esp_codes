70cb7cc63442bcd38e6508e85553c74b9abb0904
arsh0023 <arsharsh2391@gmail.com>
2021-11-07 17:55:12 +0530

    All sensors functioning API calls left

--- wifi_simple/wifi_simple.ino
+++ wifi_simple/wifi_simple.ino
@@ -4,8 +4,12 @@ const char* ssid = "Airtel_9034156560";
 const char* password = "air37671";
 
 const int led = 5;
-const int ir1 = 18; //active low.
+const int ir1 = 18; //active low. //will be for the input of the visitor.
+const int ir2 = 19; // will be for the outgoing of the visitor.
 const int temp_sensor = 34;
+
+int visitor_in = 0;
+int visitor_out = 0;
 
 void initWiFi() {
   WiFi.mode(WIFI_STA);
@@ -35,15 +39,39 @@ void loop() {
   float val = temp_val/10;
   Serial.println(val);
   int ir1_val = digitalRead(ir1);
-  if(val <= 38)
-  {
-    digitalWrite(led,HIGH);
-  }
-  else
+  int ir2_val = digitalRead(ir2);
+  if(val <= 20)
   {
     digitalWrite(led,LOW);
   }
-//  if(ir1_val == 0)
+  else
+  {
+    digitalWrite(led,HIGH);
+  }
+
+  if(ir1_val == 0)
+  {
+    visitor_in++;
+    //api call to set the visitor_in.
+  }
+  
+  if(ir2_val == 0)
+  {
+    if(visitor_in != 0)
+    {
+      visitor_out++;
+      visitor_in--;
+    }  
+    //api call to set both.
+  }
+
+//  Serial.println("visitor_in");
+//  Serial.println(visitor_in);
+//  Serial.println("visitor_out");
+//  Serial.println(visitor_out);
+  delay(1000);
+  
+//  if(ir1_val == 0) // this means when the value is 0 the person is there.
 //  {
 //    digitalWrite(led,HIGH);
 //  }
